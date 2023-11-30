dcdfe1ab04298f3e6e9dd04bc724ce09529722cc
chunchunmaru12 <princekhan456.pk10@gmail.com>
2023-11-30 11:52:09 +0545

    meow

--- daytwo/js/script.js
+++ daytwo/js/script.js
@@ -1,6 +1,36 @@
-var a = 7;
-var isprime = a == 1 ? false : true;
-for (let i = 2; i < a; i++) {
-    a % i == 0 ? isprime *= false : isprime *= true;
-};
-alert(`${a} is ${isprime ? 'prime' : 'not prime'} number`);
+/*
+Variable is in JS
+to define variable in JS we use 'var' or 'let'
+'const' is used to define constant variable and cannot be changed
+*/ 
+
+//example
+var num =12
+let user_status=true
+//to print in console
+console.log(num)
+console.log(user_status)
+
+/*NOTE: 
+1. var is use for global scope and let is use for block scope
+2. variable defined with var can be re-declared and update
+3. variable defined with let can be updated but cannot re declare
+
+
+variable re declaration 
+note: variable cannot be access before it is defined so whenever varibale is re declared
+it depends upon the scope it is being declared
+*/
+let msg="Hello All!"
+function getMessage(){
+    //u cannot access msg before let so console.log(msg) on this line wont run and show error
+    //but the same in var we can access it anywhere but the value is stored as undefined
+    let msg="Wowo"
+    console.log(msg)
+}
+Hello()
+ function Hello(){
+    console.log("za warudoo")
+}
+getMessage();
+console.log(msg)
